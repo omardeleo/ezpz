78bf707b8cbf93d3a219dfd0bf6a3c253989cbf3
Omar De Leo <omar.deleo@gmail.com>
2019-02-27 14:18:50 -0500

    Refactor code

--- src/dom_node_collection.js
+++ src/dom_node_collection.js
@@ -3,19 +3,18 @@ class DOMNodeCollection {
     this.nodes = nodes;
   }
 
-  html() {
-    if (arguments.length === 0) {
+  each(cb) {
+    this.nodes.forEach(cb);
+  }
+
+  html(html) {
+    if (typeof html === "string") {
+      this.each(node => {
+        node.innerHTML = html;
+      });
+      return this.nodes;
+    } else if (this.nodes.length > 0) {
       return this.nodes[0].innerHTML;
-    } else if (typeof arguments[0] === "string") {
-      this.nodes.forEach(node => {
-        node.innerHTML = arguments[0];
-      });
-      return this;
-    } else if (typeof arguments[0] === "function") {
-      this.nodes.forEach(el => {
-        el.innerHTML = arguments[0]();
-      });
-      return this;
     }
   }
 
@@ -43,7 +42,6 @@ class DOMNodeCollection {
 
   attr(name) {
     if (arguments.length > 1) {
-      // console.log(arguments);
       this.nodes.forEach(node => {
         node.attributes.item(name).value = arguments[1];
       });
@@ -54,35 +52,15 @@ class DOMNodeCollection {
   }
 
   addClass(className) {
-    this.nodes.forEach(node => {
-      if (node.attributes.getNamedItem("class")) {
-        node.attributes.getNamedItem("class").value += " " + className;
-      } else {
-        node.setAttribute("class", className);
-      }
-    });
-    return this;
+    this.each(node => node.classList.add(className));
   }
 
-  removeClass() {
-    if (typeof arguments[0] === "string") {
-      let remClasses = arguments[0].split(" ");
-      this.nodes.forEach(node => {
-        let classValues = node.attributes.class.value.split(" ");
-        remClasses.forEach(remClass => {
-          if (classValues.includes(remClass)){
-            classValues.splice(classValues.indexOf(remClass), 1);
-          }
-        });
-        node.attributes.class.value = classValues.join(" ");
-        if (node.attributes.class.value === "") {
-          node.removeAttribute("class");
-        }
-      });
-    } else if (typeof arguments[0] === "function") {
-      this.removeClass(arguments[0]());
-    }
-    return this;
+  removeClass(className) {
+    this.each(node => node.classList.remove(className));
+  }
+
+  toggleClass(className) {
+    this.each(node => node.classList.toggle(className))
   }
 
   // EZPZ! Traversal
@@ -125,31 +103,36 @@ class DOMNodeCollection {
 
   // EZPZ! EVENT HANDLERS
 
-    on() {
-      this.nodes.forEach(node => {
-        node.addEventListener(arguments[0], arguments[1]);
-        node.callback = arguments[1];
-      });
-      return this;
-    }
+  on() {
+    this.nodes.forEach(node => {
+      node.addEventListener(arguments[0], arguments[1]);
+      node.callback = arguments[1];
+    });
+    return this;
+  }
 
-    off() {
-      this.nodes.forEach(node => {
-        node.removeEventListener(arguments[0], node.callback);
-        node.callback = null;
-      });
-      return this;
-    }
+  off() {
+    this.nodes.forEach(node => {
+      node.removeEventListener(arguments[0], node.callback);
+      node.callback = null;
+    });
+    return this;
+  }
 
-    ready(callback) {
-      if (document.readyState === "complete") {
-        callback();
-      } else {
-        document.addEventListener("DOMContentLoaded", callback);
-      }
-      let dnc = new DOMNodeCollection(document);
-      return dnc;
+  ready(callback) {
+    if (document.readyState === "complete") {
+      callback();
+    } else {
+      document.addEventListener("DOMContentLoaded", callback);
     }
+    let dnc = new DOMNodeCollection(document);
+    return dnc;
+  }
+
+  css(property, value) {
+    this.each(node => node.style[property] = value);
+    console.log(this.nodes);
+  }
 
 }
 
